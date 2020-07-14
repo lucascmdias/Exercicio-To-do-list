0d8094b6ed70a7a3d46f18f8453a16e32d254ecb
Lucas Dias <lucascmdias@gmail.com>
2020-07-14 14:35:29 -0300

    colocando o express e o nodemon

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.send('<h1> testando :DD</h1>')
+})
+
+app.listen(3000,() =>{
+    console.log("servido ativo")
+})
