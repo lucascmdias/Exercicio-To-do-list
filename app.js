820f4de81369482897dbc33afcac4dc1caa383cf
Lucas Dias <lucascmdias@gmail.com>
2020-07-19 19:08:40 -0300

    aula até o mongoose

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express')
+const ChecklistRouter = require('./src/routes/checklist')
 const app = express()
+require('./config/database')
+
+app.use(express.json())
+
+const log = (req,res,next) =>{
+    console.log(`Data de hoje: ${Date.now()}`)
+    next()
+}
+
+app.use(log)
+
+app.use('/checklist',ChecklistRouter)
 
 app.get('/', (req,res) =>{
     res.send('<h1> testando :DD</h1>')
+})
+
+
+
+app.get('/json',(req,res) =>{
+    console.log(req.body)
+    res.json({title: 'tarefa X', done: true})
 })
 
 app.listen(3000,() =>{
