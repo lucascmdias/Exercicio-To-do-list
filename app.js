04cbfcc6f690afdc645b127b74e686e826ea3542
Lucas Dias <lucascmdias@gmail.com>
2020-07-20 13:56:10 -0300

    novas atualizações com views e mongodb finalizado

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 const express = require('express')
 const ChecklistRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
 const app = express()
 require('./config/database')
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use(express.json())
 
-const log = (req,res,next) =>{
-    console.log(`Data de hoje: ${Date.now()}`)
-    next()
-}
-
-app.use(log)
-
 app.use('/checklist',ChecklistRouter)
-
-app.get('/', (req,res) =>{
-    res.send('<h1> testando :DD</h1>')
-})
-
-
-
-app.get('/json',(req,res) =>{
-    console.log(req.body)
-    res.json({title: 'tarefa X', done: true})
-})
+app.use('/',rootRouter)
 
 app.listen(3000,() =>{
     console.log("servido ativo")
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,18 +1,60 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    console.log("ola, testando")
-    res.send()
+const Checklist = require('../models/checklist')
+
+
+router.get('/',async (req,res)=>{
+    try {
+        let checklist = await Checklist.find({})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+        
+    }
 })
 
-router.post('/',(req,res) =>{
-    console.log(req.body)
-    res.status(200).send(req.body)
+router.post('/', async (req,res) =>{
+    let { name } = req.body
+    try {
+        let checklist = await Checklist.create({name})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+        
+    }
 })
 
-router.get('/:id',(req,res) => {
-    console.log(req.params.id)
-    res.send(`ID: ${req.params.id}`)
+router.get('/:id',async (req,res) => {
+
+    try {
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
+    
 })
+
+router.put('/:id',async (req,res) => {
+    let {name} = req.body
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+        
+    }
+})
+
+router.delete('/:id',async (req,res) => {
+    try {
+        let checklist = await Checklist.findByIdAndDelete(req.params.id)
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+        
+    }
+})
+
 module.exports = router
